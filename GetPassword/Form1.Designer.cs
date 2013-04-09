9674496886048c52241c015159760634b9a82d5d
Trent Monahan <sinz@d3s.co>
2013-04-10 06:13:40 +1000

    initial commit
    All functionality intact

---
+++ GetPassword/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace GetPassword {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(13, 13);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(53, 13);
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "username";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(12, 48);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(52, 13);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 104);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "SinZational lastlogin Decoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+    }
+}
+
---
+++ GetPassword/rememberMe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Net;
+using SharpLauncher;
+using System.Windows.Forms;
+
+namespace Resolute_Launcher {
+    public class rememberMe { //Functions provided by SirCmpwn
+
+        private static readonly byte[] LastLoginSalt = new byte[] { 0x0c, 0x9d, 0x4a, 0xe4, 0x1e, 0x83, 0x15, 0xfc };
+        private const string LastLoginPassword = "passwordfile";
+
+        static String rootPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".minecraft/");
+        static String lastLoginPath = Path.Combine(rootPath, "lastlogin");
+
+        public String[] GetLastLogin() {
+            try {
+                byte[] encryptedLogin = File.ReadAllBytes(lastLoginPath);
+                PKCSKeyGenerator crypto = new PKCSKeyGenerator(LastLoginPassword, LastLoginSalt, 5, 1);
+                ICryptoTransform cryptoTransform = crypto.Decryptor;
+                byte[] decrypted = cryptoTransform.TransformFinalBlock(encryptedLogin, 0, encryptedLogin.Length);
+                short userLength = IPAddress.HostToNetworkOrder(BitConverter.ToInt16(decrypted, 0));
+                byte[] user = decrypted.Skip(2).Take(userLength).ToArray();
+                short passLength = IPAddress.HostToNetworkOrder(BitConverter.ToInt16(decrypted, userLength + 2));
+                byte[] password = decrypted.Skip(4 + userLength).ToArray();
+                String[] result = new String[2];
+                result[0] = System.Text.Encoding.UTF8.GetString(user);
+                result[1] = System.Text.Encoding.UTF8.GetString(password);
+                return result;
+            }
+            catch (Exception e){
+                MessageBox.Show(e.StackTrace);
+                return null;
+            }
+        }
+    }
+}
